c986e4fe8b04c0dc3f1119e2e0de1f6247a9eedd
Vishal Urankar <vvu060@gmail.com>
2021-01-14 15:20:47 +0530

    google-clone

---
+++ src/Search.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./Search.css";
+import SearchIcon from "@material-ui/icons/Search";
+import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { actionTypes } from "./reducer";
+
+function Search({ hideButtons = false }) {
+  const [{}, dispatch] = useStateValue();
+
+  const history = useHistory();
+  const [input, setInput] = useState("");
+
+  const search = (e) => {
+    e.preventDefault();
+    console.log("Search button Clicked", input);
+
+    dispatch({
+      type: actionTypes.SET_SEARCH_TERM,
+      term: input,
+    });
+
+    history.push("/search");
+  };
+
+  return (
+    <form className="search">
+      <div className="search__input">
+        <SearchIcon className="seach__inputIcon" />
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <MicIcon />
+      </div>
+
+      {!hideButtons ? (
+        <div className="search__buttons">
+          <Button type="submit" onClick={search} variant="outlined">
+            Google Search
+          </Button>
+          <Button variant="outlined">I'm Feeking Lucky</Button>
+        </div>
+      ) : (
+        <div className="search__buttons">
+          <Button
+            className="search__buttonsHidden"
+            type="submit"
+            onClick={search}
+            variant="outlined"
+          >
+            Google Search
+          </Button>
+          <Button className="search__buttonsHidden" variant="outlined">
+            I'm Feeking Lucky
+          </Button>
+        </div>
+      )}
+    </form>
+  );
+}
+
+export default Search;
---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyDLcDqzjx-2X0370Slfj2oOFJAHpkT1cwk";
+
+export default API_KEY;
